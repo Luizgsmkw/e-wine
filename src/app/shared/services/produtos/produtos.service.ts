a92da6a2a0a08e39562aefafb95c3283706ae8a1
JaqueSans <jaqueline.santosearaujo@mail.com>
2022-05-21 13:17:41 -0300

    Criação de interface e service de produtos

--- src/app/shared/services/produtos/produtos.service.ts
+++ src/app/shared/services/produtos/produtos.service.ts
@@ -6,6 +6,26 @@ import { Produto, CategoriaProduto } from 'src/app/models/produto';
   providedIn: 'root'
 })
 export class ProdutosService {
+private produtos: Produto[] = [
+{
+  imagem:'https://ingavinhos.agilecdn.com.br/40.png', 
+  nomeDoProduto: 'Luigi Bosca',
+  descricao: 'Um tinto puro, de grande corpo, estrutura, caráter e toda a suculência típica da variedade. Permanência longa e final de boca elegante.',
+  uva: 'malbec',
+  pais: 'Argentina',
+  preco: 190.00,
+  emPromocao: true,
+  desconto: 0.2, //0...1  
+  categoria: CategoriaProduto.TINTO,
+},
+];
 
+getProdutos(): Produto[]{
+  return this.produtos;
+}
+
+addProduto(produto: Produto) {
+  this.produtos.push(produto);
+}
   constructor() { }
 }
