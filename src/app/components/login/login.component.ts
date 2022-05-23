4acde5abbd7a58eadc6e781326f84d9f4a894201
JaqueSans <jaqueline.santosearaujo@mail.com>
2022-05-23 09:30:36 -0300

    Validação e estilização da pagina login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,30 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements 
+OnInit {
+constructor(private fb: FormBuilder) { }
 
-
-  images = ['https://cdn.pixabay.com/photo/2015/04/25/03/09/cork-738603_960_720.jpg', 'https://cdn.pixabay.com/photo/2017/03/27/13/36/champagne-2178775_960_720.jpg', 'https://cdn.pixabay.com/photo/2017/06/27/14/37/wines-2447514_960_720.jpg'];
-
-
-  email: string = ''; 
-  senha: string = '';
-
+  loginForm= this.fb.group({
+  login: ['', [Validators.required, Validators.minLength(5)]], //começa vazio, e cria validador
+  senha:['', [Validators.required, Validators.minLength(8)]]
+});
+//utiliza o get para que não seja necessário utilizar lá na frente, no ngIg um linha grande p pegar ficou só: *ngIf="nome?.touched && nome?.invalid"
+  get login(){
+    return this.loginForm.get('nome');
+  }
+  get senha(){
+    return this.loginForm.get('senha');
+  }
   onSubmit(){
-    alert(`Bem-vindo(a) ${this.email}`);
+    alert(`Seja bem-vindo! Cliente Logado`);
   }
 
-  constructor() { }
+
+  images = ['https://www.divvino.com.br/blog/wp-content/uploads/2020/08/playlist-para-tomar-vinho.jpg', 'https://www.fazfacil.com.br/wp-content/uploads/2012/11/20161227-vinhos-comidas.jpg' ];
 
   ngOnInit(): void {
   }
 
 
 }
-
-
 
